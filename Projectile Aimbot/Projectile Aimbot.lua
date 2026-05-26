17e5ecfd65ccade8478aac98e129c950abdc1ed1
AntonioRBLX <124460799+AntonioRBLX@users.noreply.github.com>
2026-05-26 16:57:24 +0100

    Update Projectile Aimbot.lua

--- Projectile Aimbot/Projectile Aimbot.lua
+++ Projectile Aimbot/Projectile Aimbot.lua
@@ -1,3 +1,4 @@
+-- MAKE THE ALGORITHM CHECK FOR BODY TYPES EG. SIZE, WIDTH, DEPTH OF A PLAYER
 local module = {}
 function module:Compute(startPosition,targetCharacter,projectileSpeed,projectileGravity,argumentTable)
 	local LocalPlayer
@@ -37,12 +38,13 @@ function module:Compute(startPosition,targetCharacter,projectileSpeed,projectile
 	local aimHeight = argumentTable.AimHeight or 0
 	local isAGun = argumentTable.IsAGun or false
 	local ignoreList = argumentTable.IgnoreList or {}
-	for _, v in pairs(ignoreList) do
+	for _, v in ignoreList do
 		assert(typeof(v) == "Instance","Ignore list elements must be Instances")
 	end
 	local interval = argumentTable.Interval or 1/80
 	local maxSimulationTime = argumentTable.MaxSimulationTime or 60
 	local respectCanCollide = argumentTable.RespectCanCollide == nil and true or argumentTable.RespectCanCollide
+	local asynchronous = if argumentTable.Asynchronous == nil then false else argumentTable.Asynchronous
 	assert(interval > 0,"INTERVAlS CANNOT NOT BE 0 NOR NEGATIVE")
 	assert(ping >= 0,"PING CANNOT BE NEGATIVE")
 	assert(typeof(ignoreList) == "table","IgnoreList must be a table")
@@ -58,6 +60,7 @@ function module:Compute(startPosition,targetCharacter,projectileSpeed,projectile
 	assert(typeof(isAGun) == "boolean","IsAGun must be a boolean")
 	assert(typeof(interval) == "number","Interval must be a number")
 	assert(typeof(maxSimulationTime) == "number","Maximum Calculations must be a number")
+	assert(typeof(asynchronous) == "boolean","Asynchronous must be a boolean")
 	local startTick = tick()
 	local originalInterval = interval
 	local simulatedPos = targetRoot.Position
@@ -75,17 +78,17 @@ function module:Compute(startPosition,targetCharacter,projectileSpeed,projectile
 	local travelTime
 	local x
 	local y
-	local floorHit = Instance.new("Part", workspace)
+	local floorHit = Instance.new("Part")
 	floorHit.Name = "floorHit"
 	floorHit.Size = Vector3.new(2,2,1)
 	floorHit.CFrame = CFrame.new(targetRoot.Position - Vector3.new(0,2,0)) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0)
 	floorHit.Anchored = true
-	local wallHit = Instance.new("Part", workspace)
+	local wallHit = Instance.new("Part")
 	wallHit.Name = "wallHit"
 	wallHit.Size = Vector3.new(2,2,1)
 	wallHit.CFrame = CFrame.new(targetRoot.Position) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0)
 	wallHit.Anchored = true
-	local ceilHit = Instance.new("Part", workspace)
+	local ceilHit = Instance.new("Part")
 	ceilHit.Name = "ceilHit"
 	ceilHit.Size = Vector3.new(2,1,1)
 	ceilHit.CFrame = CFrame.new(targetRoot.Position + Vector3.new(0,1.5,0)) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0)
@@ -100,15 +103,15 @@ function module:Compute(startPosition,targetCharacter,projectileSpeed,projectile
 		local touchingParts = {}
 		local touchingTrussLadder = false
 		local function checkInsideIgnoreList(obj)
-			for _, v in pairs(ignoreDescendantsInstances) do
+			for _, v in ignoreDescendantsInstances do
 				if obj:IsDescendantOf(v) then
 					return true
 				end
 			end
 			return false
 		end
-		for _, v in pairs(tab) do
-			if not table.find(ignoreDescendantsInstances,v) and v ~= floorHit and v ~= wallHit and v ~= ceilHit and not v:IsDescendantOf(targetCharacter) and (not LocalPlayer.Character or not v:IsDescendantOf(LocalPlayer.Character)) and not checkInsideIgnoreList(v) and (not respectCanCollide or v.CanCollide) then
+		for _, v in tab do
+			if not table.find(ignoreDescendantsInstances,v) and v ~= floorHit and v ~= wallHit and v ~= ceilHit and not v:IsDescendantOf(targetCharacter) and (not LocalPlayer or not LocalPlayer.Character or not v:IsDescendantOf(LocalPlayer.Character)) and not checkInsideIgnoreList(v) and (not respectCanCollide or v.CanCollide) then
 				table.insert(touchingParts,v)
 				if v.ClassName == "TrussPart" then
 					touchingTrussLadder = true
@@ -210,7 +213,7 @@ function module:Compute(startPosition,targetCharacter,projectileSpeed,projectile
 		local params = RaycastParams.new()
 		params.FilterDescendantsInstances = {obj}
 		params.FilterType = Enum.RaycastFilterType.Include
-		local blockcast = workspace:Blockcast(CFrame.new(Vector3.new(simulatedPos.X,simulatedPos.Y + dirY / math.abs(dirY),simulatedPos.Z)) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0),Vector3.new(2, 0.002, 1),Vector3.new(0,dirY,0),params)
+		local blockcast = workspace:Blockcast(CFrame.new(Vector3.new(simulatedPos.X,simulatedPos.Y + dirY / math.abs(dirY),simulatedPos.Z)) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0),Vector3.new(2,0,1),Vector3.new(0,dirY,0),params)
 		if blockcast and blockcast.Position then
 			return blockcast.Position.Y
 		end
@@ -220,108 +223,120 @@ function module:Compute(startPosition,targetCharacter,projectileSpeed,projectile
 			moveDirection = CFrame.new(targetRoot.Position * Vector3.new(1,0,1), walkToPoint * Vector3.new(1,0,1)).LookVector
 		end
 	end
-	simulationIgnoreList = {floorHit,wallHit,ceilHit,targetCharacter}
-	if LocalPlayer.Character then
-		table.insert(simulationIgnoreList,LocalPlayer.Character)
-	end
-	while true do
-		local simulationTick = tick()
-		local tickDifference = predictLag and (simulationTick - startTick) or 0
-		simulatedPos += Vector3.new(0,aimHeight,0)
-		x = (Vector2.new(simulatedPos.X,simulatedPos.Z) - Vector2.new(startPosition.X,startPosition.Z)).Magnitude
-		y = simulatedPos.Y - startPosition.Y
-		launchAngle = projectileGravity ~= 0 and math.atan((v^2 - math.sqrt(v^4 - pg*(pg*x^2 + 2*y*v^2))) / (pg*x)) or math.atan(y/x)
-		travelTime = isAGun and ping / 1000 + tickDifference or x / math.cos(launchAngle) / v + ping / 1000 + tickDifference
-		interval = travelTime - simulatedTime <= interval and travelTime - simulatedTime or originalInterval	
-		simulatedPos -= Vector3.new(0,aimHeight,0)
-		if simulatedTime >= travelTime or simulatedTime >= maxSimulationTime or simulatedPos.Y <= workspace.FallenPartsDestroyHeight then break end
-		prevSimulatedPos = simulatedPos
-		moveDirection = walkToPoint ~= Vector3.new(0,0,0) and CFrame.new(targetRoot.Position * Vector3.new(1,0,1), walkToPoint * Vector3.new(1,0,1)).LookVector or moveDirection
-		simulationStep()
-		simulatedVel -= Vector3.new(0,gravity*interval,0)
-		local raycastParams = RaycastParams.new()
-		raycastParams.FilterDescendantsInstances = simulationIgnoreList
-		raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
-		raycastParams.RespectCanCollide = respectCanCollide
-		local checkFloorIntercept = workspace:Blockcast(CFrame.new(prevSimulatedPos - Vector3.new(0,1.001,0)) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0),Vector3.new(2, 0.002, 1),(prevSimulatedPos - Vector3.new(0,1.001,0)) - (simulatedPos - Vector3.new(0,1.001,0)),raycastParams)
-		local checkCeilIntercept = workspace:Blockcast(CFrame.new(prevSimulatedPos + Vector3.new(0,1.001,0)) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0),Vector3.new(2, 0.002, 1),(prevSimulatedPos + Vector3.new(0,1.001,0)) - (simulatedPos + Vector3.new(0,1.001,0)),raycastParams)
-		if checkFloorIntercept and checkFloorIntercept.Position and checkFloorIntercept.Position.Y <= prevSimulatedPos.Y then
-			simulatedPos = Vector3.new(simulatedPos.X,checkFloorIntercept.Position.Y + 3,simulatedPos.Z)
-		elseif checkCeilIntercept and checkCeilIntercept.Position and checkCeilIntercept.Position.Y >= prevSimulatedPos.Y then
-			simulatedPos = Vector3.new(simulatedPos.X,checkCeilIntercept.Position.Y - 2,simulatedPos.Z)
-		end
-		updateIndicatorPosition()
-		local wallTouchingParts, touchingTrussLadder = checkTouchingParts(wallHit:GetTouchingParts(),ignoreList)
-		if #wallTouchingParts > 0 then
-			if touchingTrussLadder then
-				simulatedVel = Vector3.new(0,targetHum.WalkSpeed,0)
-			else
-				local dir = CFrame.new(prevSimulatedPos,simulatedPos).LookVector.Unit
-				if dir ~= dir then dir = Vector3.new(0,0,0) end
-				simulatedPos += Vector3.new(-dir.X,0,0)
-				updateIndicatorPosition()
-				local wallTouchingParts = checkTouchingParts(wallHit:GetTouchingParts(),ignoreList)
-				if #wallTouchingParts > 0 then
-					simulatedPos += Vector3.new(dir.X,0,-dir.Z)
-					updateIndicatorPosition()
-					local wallTouchingParts = checkTouchingParts(wallHit:GetTouchingParts(),ignoreList)
-					if #wallTouchingParts > 0 then
-						simulatedPos = prevSimulatedPos
-					end
-				end
+	simulationIgnoreList = tableConcat(ignoreList,{targetCharacter})
+	--if LocalPlayer and LocalPlayer.Character then
+	--	table.insert(simulationIgnoreList,LocalPlayer.Character)
+	--end
+	local spawn = tick()
+	local computepathcoroutine = coroutine.create(function()
+		while true do
+			local simulationTick = tick()
+			local tickDifference = predictLag and (simulationTick - startTick) or 0
+			simulatedPos += Vector3.new(0,aimHeight,0)
+			x = (Vector2.new(simulatedPos.X,simulatedPos.Z) - Vector2.new(startPosition.X,startPosition.Z)).Magnitude
+			y = simulatedPos.Y - startPosition.Y
+			launchAngle = projectileGravity ~= 0 and math.atan((v^2 - math.sqrt(v^4 - pg*(pg*x^2 + 2*y*v^2))) / (pg*x)) or math.atan(y/x)
+			travelTime = isAGun and ping / 1000 + tickDifference or x / math.cos(launchAngle) / v + ping / 1000 + tickDifference
+			interval = travelTime - simulatedTime <= interval and travelTime - simulatedTime or originalInterval	
+			simulatedPos -= Vector3.new(0,aimHeight,0)
+			if simulatedTime >= travelTime or simulatedTime >= maxSimulationTime or simulatedPos.Y <= workspace.FallenPartsDestroyHeight then break end
+			prevSimulatedPos = simulatedPos
+			moveDirection = walkToPoint ~= Vector3.new(0,0,0) and CFrame.new(targetRoot.Position * Vector3.new(1,0,1), walkToPoint * Vector3.new(1,0,1)).LookVector or moveDirection
+			simulationStep()
+			simulatedVel -= Vector3.new(0,gravity*interval,0)
+			local raycastParams = RaycastParams.new()
+			raycastParams.FilterDescendantsInstances = simulationIgnoreList
+			raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+			raycastParams.RespectCanCollide = respectCanCollide
+			local checkFloorIntercept = workspace:Blockcast(CFrame.new(prevSimulatedPos - Vector3.yAxis) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0),Vector3.new(2,0,1),(prevSimulatedPos - Vector3.yAxis) - (simulatedPos - Vector3.new(0,1.00001,0)),raycastParams)
+			local checkCeilIntercept = workspace:Blockcast(CFrame.new(prevSimulatedPos + Vector3.yAxis) * CFrame.Angles(0,math.rad(targetRoot.Orientation.Y),0),Vector3.new(2,0,1),(prevSimulatedPos + Vector3.yAxis) - (simulatedPos + Vector3.new(0,1.00001,0)),raycastParams)
+			if checkFloorIntercept and checkFloorIntercept.Position and checkFloorIntercept.Position.Y <= prevSimulatedPos.Y then
+				simulatedPos = Vector3.new(simulatedPos.X,checkFloorIntercept.Position.Y + 3,simulatedPos.Z)
+			elseif checkCeilIntercept and checkCeilIntercept.Position and checkCeilIntercept.Position.Y >= prevSimulatedPos.Y then
+				simulatedPos = Vector3.new(simulatedPos.X,checkCeilIntercept.Position.Y - 2,simulatedPos.Z)
 			end
-		end
-		updateIndicatorPosition()
-		local floorTouchingParts, touchingTrussLadder = checkTouchingParts(floorHit:GetTouchingParts(),ignoreList)
-		if not touchingTrussLadder and #floorTouchingParts > 0 and simulatedVel.Y <= interval then
-			local highest
-			for _, v in pairs(floorTouchingParts) do
-				if v.ClassName == "Part" and v.Shape == Enum.PartType.Block and checkOrientation(v) and simulatedPos.Y - 1 > v.Position.Y + v.Size.Y / 2 and (not highest or v.Position.Y + v.Size.Y / 2 > highest) then
-					highest = v.Position.Y + v.Size.Y / 2
+			updateIndicatorPosition()
+			local wallTouchingParts, touchingTrussLadder = checkTouchingParts(workspace:GetPartsInPart(wallHit),simulationIgnoreList)
+			if #wallTouchingParts > 0 then
+				if touchingTrussLadder then
+					simulatedVel = Vector3.new(0,targetHum.WalkSpeed,0)
 				else
-					local height = checkHighestLowest(v,1)
-					if height and (not highest or height > highest) then
-						highest = height
+					local dir = CFrame.new(prevSimulatedPos,simulatedPos).LookVector.Unit
+					if dir ~= dir then dir = Vector3.new(0,0,0) end
+					simulatedPos += Vector3.new(-dir.X,0,0)
+					updateIndicatorPosition()
+					local wallTouchingParts = checkTouchingParts(workspace:GetPartsInPart(wallHit),simulationIgnoreList)
+					if #wallTouchingParts > 0 then
+						simulatedPos += Vector3.new(dir.X,0,-dir.Z)
+						updateIndicatorPosition()
+						local wallTouchingParts = checkTouchingParts(workspace:GetPartsInPart(wallHit),simulationIgnoreList)
+						if #wallTouchingParts > 0 then
+							simulatedPos = prevSimulatedPos
+						end
 					end
 				end
 			end
-			if highest then
-				frictionDeceleration = 750
-				simulatedPos = Vector3.new(simulatedPos.X,highest + 3,simulatedPos.Z)
-				simulatedVel *=  Vector3.new(1,0,1)
-				if predictJump and targetHum.Jump or alwaysJumping then
-					simulatedVel = Vector3.new(simulatedVel.X,playerJumpPower,simulatedVel.Z)
+			updateIndicatorPosition()
+			local floorTouchingParts, touchingTrussLadder = checkTouchingParts(workspace:GetPartsInPart(floorHit),simulationIgnoreList)
+			if not touchingTrussLadder and #floorTouchingParts > 0 and simulatedVel.Y <= interval then
+				local highest
+				for _, v in floorTouchingParts do
+					if v.ClassName == "Part" and v.Shape == Enum.PartType.Block and checkOrientation(v) and simulatedPos.Y - 1 > v.Position.Y + v.Size.Y / 2 and (not highest or v.Position.Y + v.Size.Y / 2 > highest) then
+						highest = v.Position.Y + v.Size.Y / 2
+					else
+						local height = checkHighestLowest(v,1)
+						if height and (not highest or height > highest) then
+							highest = height
+						end
+					end
 				end
-			else
+				if highest then
+					frictionDeceleration = 750
+					simulatedPos = Vector3.new(simulatedPos.X,highest + 3,simulatedPos.Z)
+					simulatedVel *=  Vector3.new(1,0,1)
+					if predictJump and targetHum.Jump or alwaysJumping then
+						simulatedVel = Vector3.new(simulatedVel.X,playerJumpPower,simulatedVel.Z)
+					end
+				else
+					frictionDeceleration = 142
+				end
+			end
+			updateIndicatorPosition()
+			local ceilTouchingParts, touchingTrussLadder = checkTouchingParts(workspace:GetPartsInPart(ceilHit),simulationIgnoreList)
+			if not touchingTrussLadder and #ceilTouchingParts > 0 and simulatedVel.Y >= -interval then
+				local lowest
+				for _, v in ceilTouchingParts do
+					if (v.ClassName == "Part" or v.ClassName == "SpawnLocation") and v.Shape == Enum.PartType.Block and checkOrientation(v) and simulatedPos.Y + 2 < v.Position.Y - v.Size.Y / 2 and (not lowest or v.Position.Y - v.Size.Y / 2 < lowest) then
+						lowest = v.Position.Y - v.Size.Y / 2
+					else
+						local low = checkHighestLowest(v,2)
+						if low and (not lowest or low < lowest) then
+							lowest = low
+						end
+					end
+				end
+				if lowest then
+					simulatedPos = Vector3.new(simulatedPos.X,lowest - 2,simulatedPos.Z)
+					simulatedVel *=  Vector3.new(1,-1,1)
+				end
+			end
+			if #floorTouchingParts <= 0 and #ceilTouchingParts <= 0 then
 				frictionDeceleration = 142
 			end
-		end
-		updateIndicatorPosition()
-		local ceilTouchingParts, touchingTrussLadder = checkTouchingParts(ceilHit:GetTouchingParts(),ignoreList)
-		if not touchingTrussLadder and #ceilTouchingParts > 0 and simulatedVel.Y >= -interval then
-			local lowest
-			for _, v in pairs(floorTouchingParts) do
-				if (v.ClassName == "Part" or v.ClassName == "SpawnLocation") and v.Shape == Enum.PartType.Block and checkOrientation(v) and simulatedPos.Y + 1.9 < v.Position.Y - v.Size.Y / 2 and (not lowest or v.Position.Y - v.Size.Y / 2 < lowest) then
-					lowest = v.Position.Y - v.Size.Y / 2
-				else
-					local low = checkHighestLowest(v,2)
-					if low and (not lowest or low < lowest) then
-						lowest = low
-					end
-				end
+			if travelTime - simulatedTime >= originalInterval then
+				table.insert(path,simulatedPos)
 			end
-			if lowest then
-				simulatedPos = Vector3.new(simulatedPos.X,lowest - 2,simulatedPos.Z)
-				simulatedVel *=  Vector3.new(1,-1,1)
+			simulatedTime += interval
+			if asynchronous and spawn + 1/60 < tick() then
+				coroutine.yield()
 			end
 		end
-		if #floorTouchingParts <= 0 and #ceilTouchingParts <= 0 then
-			frictionDeceleration = 142
-		end
-		if travelTime - simulatedTime >= originalInterval then
-			table.insert(path,simulatedPos)
-		end
-		simulatedTime += interval
+	end)
+	coroutine.resume(computepathcoroutine)
+	while coroutine.status(computepathcoroutine) ~= "dead" do
+		task.wait()
+		spawn = tick()
+		coroutine.resume(computepathcoroutine)
 	end
 	floorHit:Destroy()
 	wallHit:Destroy()
